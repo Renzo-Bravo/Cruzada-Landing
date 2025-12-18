12e5eec32f264f3fdb5d1a68786c18aa1e0a020c
Renzo-Bravo <renzobravogomez@gmail.com>
2025-12-17 21:02:07 -0300

    Update areas and info component

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,15 +1,16 @@
 import fediap from "@/assets/fediap/fediap-logo.png"
-import Gaurdapampa from "../guardapampa/Guardapampa.jsx";
+import Guardapampa from "../guardapampa/Guardapampa.jsx";
 import Line from "../line/Line.jsx";
 import "./Info.css";
 
 export default function Info() {
     return (
         <section className="info-section" id="info-id">
-            <Gaurdapampa />
+            <Guardapampa />
             <img src={fediap} alt="Fedial" className="fediap" />
             <p className="text">Las Jornadas Regionales de FEDIAP Destinado a docentes y técnicos de escuelas agrotécnicas de todo el país, durante tres días intensivos se realizarán actividades de exposición de experiencias, presentación de casos, disertaciones y networking.</p>
             <Line />
         </section>
     );
 }
+
